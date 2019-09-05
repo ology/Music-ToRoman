d136e341087b6aabd6ad13c7aa431dd9995badca
Gene Boggs <gene@ology.net>
2019-09-05 00:35:30 -0700

    Increment version

--- lib/Music/ToRoman.pm
+++ lib/Music/ToRoman.pm
@@ -2,7 +2,7 @@ package Music::ToRoman;
 
 # ABSTRACT: Convert notes and chords to Roman numeral notation
 
-our $VERSION = '0.1106';
+our $VERSION = '0.1200';
 
 use List::MoreUtils qw/ any first_index /;
 use Moo;
