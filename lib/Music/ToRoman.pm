608fc7e7b4fe0d5b621d9e78bb71ffffb8b7ccb7
Gene Boggs <gene@ology.net>
2019-09-05 19:32:07 -0700

    Improve SYNOPSIS

--- lib/Music/ToRoman.pm
+++ lib/Music/ToRoman.pm
@@ -24,20 +24,21 @@ use namespace::clean;
 
   my $roman = $mtr->parse('Am');  # i (minor)
   $roman = $mtr->parse('Bdim');   # iio (diminished)
-  $roman = $mtr->parse('B dim');  # ii o "
-  $roman = $mtr->parse('Bo');     # iio "
-  $roman = $mtr->parse('Bø');     # iio "
+  $roman = $mtr->parse('B dim');  # ii o
+  $roman = $mtr->parse('Bo');     # iio
+  $roman = $mtr->parse('Bø');     # iio
   $roman = $mtr->parse('Bb');     # bII (flat-two major)
   $roman = $mtr->parse('CM');     # III (major)
-  $roman = $mtr->parse('C');      # III "
-  $roman = $mtr->parse('Cm9/G');  # iii9/VII (minor ninth with seven bass)
-  $roman = $mtr->parse('Cm9/Bb'); # iii9/bii (minor ninth with flat-two bass)
-  $roman = $mtr->parse('D sus4'); # IV sus4 (major suspended)
-  $roman = $mtr->parse('DMaj7');  # IVmaj7 (major seventh)
-  $roman = $mtr->parse('D△7');    # IV maj7 "
+  $roman = $mtr->parse('C');      # III
+  $roman = $mtr->parse('Cm9/G');  # iii9/VII (minor-nine with seven bass)
+  $roman = $mtr->parse('Cm9/Bb'); # iii9/bii (minor-nine with flat-two bass)
+  $roman = $mtr->parse('Dsus4');  # IVsus4 (suspended)
+  $roman = $mtr->parse('D sus4'); # IV sus4
+  $roman = $mtr->parse('D maj7'); # IV maj7 (major seventh)
+  $roman = $mtr->parse('DMaj7');  # IVmaj7
+  $roman = $mtr->parse('D△7');    # IVmaj7
   $roman = $mtr->parse('E7');     # V7 (dominant seventh)
-  $roman = $mtr->parse('Em7');    # v7 (minor seventh)
-  $roman = $mtr->parse('Fmin7');  # vimin7 "
+  $roman = $mtr->parse('Fmin7');  # vimin7 (minor seventh)
   $roman = $mtr->parse('G+');     # VII+ (augmented)
 
   $mtr = Music::ToRoman->new(
